a6fe0ac6877c0673057e676b468e7671309a37b0
Phoenix <Phoenix500526@163.com>
2020-10-29 10:53:00 +0800

    添加 Callback 的实现
    在 base/TypeCast.h 中添加了智能指针的安全向下转型

--- base/TypeCast.h
+++ base/TypeCast.h
@@ -2,10 +2,8 @@
 #define TMUDUO_BASE_TYPESCAST_H_
 
 #include <string.h>  // memset
-
-#ifndef NDEBUG
 #include <assert.h>
-#endif
+#include <memory>
 
 namespace tmuduo {
 
@@ -30,24 +28,50 @@ inline void memZero(void* p, size_t n) { memset(p, 0, n); }
 // 当不小心将 bot 的类型改成了 Top
 // 类型则也能通过编译，但会引发未定义行为。这是因为 static_cast
 // 在对自定义类型进行转型时不会进行类型检查
-template <typename TO, typename FROM>
-inline TO implicit_cast(FROM const& f) {
+template <typename To, typename From>
+inline To implicit_cast(From const& f) {
   return f;
 }
 
 // down_cast 只接受指针类型的向下转型,
-template <typename TO, typename FROM>
-inline TO down_cast(FROM* f) {
-  // 利用 implicit_cast 进行反向检查以确保 TO 是 FROM 的子类
+template <typename To, typename From>
+inline To down_cast(From* f) {
+  // 利用 implicit_cast 进行反向检查以确保 To 是 From 的子类
   // 条件if(false)结合模板可以实现编译期类型检查的功能，同时由于编译器会进行优化，因此不会负上任何性能上的开销
   if (false) {
-    implicit_cast<FROM*, TO>(0);
+    implicit_cast<From*, To>(0);
   }
-#if !defined(NDEBUG) && !defined(GOOGLE_PROTOBUF_NO_RTTI)
+#if !defined(NDEBUG) && !defined(GOOGLE_PROToBUF_NO_RTTI)
   assert(f == nullptr ||
-         dynamic_cast<TO>(f) != nullptr);  // RTTI: debug mode only!
+         dynamic_cast<To>(f) != nullptr);  // RTTI: debug mode only!
 #endif
-  return static_cast<TO>(f);
+  return static_cast<To>(f);
+}
+
+//对于以下两个函数模板的使用存疑：究竟是出于什么目的才要使用模板封装已有的 get 函数
+template<typename T>
+inline T* get_pointer(const std::shared_ptr<T>& ptr)
+{
+  return ptr.get();
+}
+
+template<typename T>
+inline T* get_pointer(const std::unique_ptr<T>& ptr)
+{
+  return ptr.get();
+}
+
+//down_pointer_cast 用于对将指向基类的智能指针向下转型为子类指针
+template<typename To, typename From>
+inline ::std::shared_ptr<To> down_pointer_cast(const ::std::shared_ptr<From>& f){
+    if (false)
+    {
+        implicit_cast<From*, To*>(0);
+    }
+#ifndef NDEBUG
+    assert(f == nullptr || dynamic_cast<To*>(get_pointer(f)) != nullptr);
+#endif
+    return ::std::static_pointer_cast<To>(f);
 }
 
 }  // namespace tmuduo
---
+++ net/Callbacks.h
@@ -0,0 +1,35 @@
+#ifndef TMUDUO_NET_CALLBACKS_H_
+#define TMUDUO_NET_CALLBACKS_H_
+
+#include "base/Timestamp.h"
+
+#include <functional>
+#include <memory>
+
+namespace tmuduo{
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+
+namespace net{
+
+class Buffer;
+class TcpConnection;
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+using TimerCallback = std::function<void()>;
+using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
+using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
+using WriteCompleteCallback = std::function<void(const TcpConnectionPtr&)>;
+using HighWaterMarkCallback = std::function<void(const TcpConnectionPtr&, size_t)>;
+
+// the data has been read to (buf, len)
+using MessageCallback = std::function<void(const TcpConnectionPtr&, Buffer*, Timestamp)>;
+
+void defaultConnectionCallback(const TcpConnectionPtr& conn);
+void defaultMessageCallback(const TcpConnectionPtr& conn, Buffer* buffer, Timestamp receiveTime);
+
+}   //namespace net
+}   //namespace tmuduo
+
+#endif  //TMUDUO_NET_CALLBACKS_H_
