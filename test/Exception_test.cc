4e19c73688fb9844f2557a16edeb07a30610aa94
Phoenix <Phoenix500526@163.com>
2020-11-07 10:09:24 +0800

    修改 Exception_test，利用命令行参数个数来决定打印哪种异常信息

--- test/Exception_test.cc
+++ test/Exception_test.cc
@@ -6,37 +6,40 @@
 
 class Bar {
  public:
-  void test(std::vector<std::string> names = {}) {
-    printf("Stack:\n%s\n", tmuduo::CurrentThread::stackTrace(true).c_str());
-    [] {
+  void test(bool flag, std::vector<std::string> names = {}) {
+    printf("Stack:\n%s\n", tmuduo::CurrentThread::stackTrace(flag).c_str());
+    [flag] {
       printf("Stack inside lambda:\n%s\n",
-             tmuduo::CurrentThread::stackTrace(true).c_str());
+             tmuduo::CurrentThread::stackTrace(flag).c_str());
     }();
-    std::function<void()> func([] {
+    std::function<void()> func([flag] {
       printf("Stack inside std::function:\n%s\n",
-             tmuduo::CurrentThread::stackTrace(true).c_str());
+             tmuduo::CurrentThread::stackTrace(flag).c_str());
     });
     func();
-    func = std::bind(&Bar::callback, this);
+    func = std::bind(&Bar::callback, this, flag);
     func();
     throw tmuduo::Exception("oops");
   }
 
  private:
-  void callback() {
+  void callback(bool flag) {
     printf("Stack inside std::bind:\n%s\n",
-           tmuduo::CurrentThread::stackTrace(true).c_str());
+           tmuduo::CurrentThread::stackTrace(flag).c_str());
   }
 };
 
-void foo() {
+void foo(bool flag) {
   Bar b;
-  b.test();
+  b.test(flag);
 }
 
-int main() {
+int main(int agrc, char* agrv[]) {
   try {
-    foo();
+    if (agrc > 1) {
+      foo(false);
+    }
+    foo(true);
   } catch (const tmuduo::Exception& ex) {
     printf("reason:%s\n", ex.what());
     printf("stack trace:\n%s\n", ex.stackTrace());
